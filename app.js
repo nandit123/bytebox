cffce2b572ab26efdea71b381215b1b811ef3b96
Nandit <nanditmehra123@gmail.com>
2020-09-24 01:36:38 +0530

    google drive connected and files listing

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.send('Hello World2!')
-})
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
+app.set('view engine', 'ejs'); 
+var indexRouter = require('./routes/index.js')
+
+app.use('/', indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+router.get('/', function(req, res) {
+  res.render('index', {name: "Byte me"});
+});
+
+module.exports = router;
